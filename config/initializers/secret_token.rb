3f1d732121466b8a1a98e927006744511bda5ef6
Miles Z. Sterrett <miles@mileszs.com>
2012-05-09 23:24:17 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VimtweetsCom::Application.config.secret_token = 'e5df89fe0bc5f01e65d7042a697e8a743b09eef0db24784d370a0f37011dfc2440939deecc4d82203af54a3d447cd05ce44cdbe9204e2fa70ed67f0fd525a977'
