465a466476720caae3b991494dc0a8f67104f3a8
Miles Z. Sterrett <miles@mileszs.com>
2013-01-28 15:35:50 -0500

    Proper disabling fuck.

--- config/initializers/disable_xml_and_yml_parsing.rb
+++ config/initializers/disable_xml_and_yml_parsing.rb
@@ -1,2 +1,5 @@
-ActiveSupport::CoreExtensions::Hash::Conversions::XML_PARSING.delete('symbol')
-ActiveSupport::CoreExtensions::Hash::Conversions::XML_PARSING.delete('yaml')
+ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::XML)
+ActionDispatch::ParamsParser::DEFAULT_PARSERS.delete(Mime::YAML)
+
+ActiveSupport::XmlMini::PARSING.delete("symbol")
+ActiveSupport::XmlMini::PARSING.delete("yaml")
